87f351351f7b9090ee38b7ce67363ef92833c52d
kabelbaan <bosjoost201@gmail.com>
2020-03-04 12:10:07 +0100

    'setup'

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->prefix('v1')->middleware('auth:api')->group(function() {
+    Route::get('users/me', 'UsersApiController@me');
+});
