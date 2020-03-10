e4f5956b63cb54d0ba06d2e57bf65a9fbbf90321
Kevin Fillet <KevinFilletCobos@gmail.com>
2020-03-10 10:19:52 +0100

    Create post request room

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -31,11 +31,11 @@ class RoomController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
