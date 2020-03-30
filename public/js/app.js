2f8cea85e59f4979a53878ae6c8e6ca8f2e4d938
kristan frauenfelder <kfrauenf2827@student.landstede.nl>
2020-03-30 14:44:46 +0200

    save

--- public/js/app.js
+++ public/js/app.js
@@ -83795,8 +83795,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "",
-  cluster: "mt1",
+  key: "907c0cc5fd80d95efe06",
+  cluster: "eu",
   encrypted: true
 });
 
