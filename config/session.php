d3b87e8372be882604e0fa155db82f45016f4526
Student Joost Bos <jbos1135@student.landstede.nl>
2020-04-15 10:39:56 +0200

    redux to store

--- config/session.php
+++ config/session.php
@@ -93,12 +93,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | When using the "apc", "memcached", or "dynamodb" session drivers you may
-    | list a cache redux that should be used for these sessions. This value
+    | list a cache store that should be used for these sessions. This value
     | must match with one of the application's configured cache "stores".
     |
     */
 
-    'redux' => env('SESSION_STORE', null),
+    'store' => env('SESSION_STORE', null),
 
     /*
     |--------------------------------------------------------------------------
