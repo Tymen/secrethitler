b1c6edd908da8de9a14a2e3f2935cd3440dc90c3
Tymen Vis <Tymenvis@outlook.com>
2020-05-12 14:30:39 +0200

    d

--- app/Events/KickUserEvent.php
+++ app/Events/KickUserEvent.php
@@ -2,7 +2,6 @@
 
 namespace App\Events;
 
-use App\Room;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -11,7 +10,6 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-<<<<<<< HEAD:app/Events/KickUserEvent.php
 class KickUserEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
@@ -19,30 +17,16 @@ class KickUserEvent implements ShouldBroadcast
     public $roomId;
     public $userId;
 
-=======
-class setPolicyEvent implements ShouldBroadcast
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-    public $roomID;
-    public $policy;
->>>>>>> Changed some validation and added events:app/Events/setPolicyEvent.php
     /**
      * Create a new event instance.
      *
      * @param $roomId
      * @param $userId
      */
-<<<<<<< HEAD:app/Events/KickUserEvent.php
     public function __construct($roomId, $userId)
     {
         $this->roomId = $roomId;
         $this->userId = $userId;
-=======
-    public function __construct(Room $room)
-    {
-        $this->roomID = $room->id;
-        $this->policy = [$room->roomState->chosen_policies];
->>>>>>> Changed some validation and added events:app/Events/setPolicyEvent.php
     }
 
     /**
@@ -52,15 +36,11 @@ class setPolicyEvent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PresenceChannel("room.{$this->roomID}");
+        return new PresenceChannel("room.{$this->roomId}");
     }
 
     public function broadcastAs()
     {
-<<<<<<< HEAD:app/Events/KickUserEvent.php
         return 'user-kicked';
-=======
-        return "get-policy";
->>>>>>> Changed some validation and added events:app/Events/setPolicyEvent.php
     }
 }
