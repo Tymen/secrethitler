3e9a0a5a54e0393bdd2b7a2f8a35c032f36015ad
Tymen Vis <Tymenvis@outlook.com>
2020-05-26 12:36:18 +0200

    d

--- resources/js/components/Universal/Nav.js
+++ resources/js/components/Universal/Nav.js
@@ -40,11 +40,14 @@ class Nav extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <Link className="nav-item" to="/">
-                            <li className="nav-link">Home</li>
+                        <Link className="navbar-brand" to="/">
+                            <li className="navbar-logo text-center"><img src="/images/Secrethitler-no-bg.png"></img></li>
                         </Link>
                     </ul>
                     <ul className="navbar-nav ml-auto">
+                        <Link className="nav-item" to="/">
+                            <li className="nav-link">Home</li>
+                        </Link>
                         <Link className="nav-item" to="/gamerules">
                             <li className="nav-link">Gamerules</li>
                         </Link>
